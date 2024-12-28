b10c6446beda35b124b96b927563c9d2f9e80e14
Rafael Sulimann <sulimannrafael@gmail.com>
2024-12-28 03:47:07 -0300

    Criação do projeto

---
+++ src/main/java/com/sulimann/projetoddd/ProjetodddApplication.java
@@ -0,0 +1,13 @@
+package com.sulimann.projetoddd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetodddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetodddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sulimann/projetoddd/ProjetodddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sulimann.projetoddd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetodddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

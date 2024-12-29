1698da0cbb30f7303058e88e7628f6621096cd49
Rafael Sulimann <sulimannrafael@gmail.com>
2024-12-29 18:01:39 -0300

    Product, ProductTest - criando entidade de produto e implementado seus testes unitários.

---
+++ src/main/java/com/sulimann/projetoddd/entity/Product.java
@@ -0,0 +1,28 @@
+package com.sulimann.projetoddd.entity;
+
+import static com.sulimann.projetoddd.shared.AssertUtils.hasText;
+import static com.sulimann.projetoddd.shared.AssertUtils.isTrue;
+import static com.sulimann.projetoddd.shared.AssertUtils.notNull;
+
+import java.math.BigDecimal;
+
+public class Product {
+
+  private String id;
+  private String name;
+  private BigDecimal price;
+
+  public Product(String id, String name, BigDecimal price) {
+    this.id = id;
+    this.name = name;
+    this.price = price;
+    this.validate();
+  }
+
+  private void validate() {
+    hasText(this.id, "Id is required");
+    hasText(this.name, "Name is required");
+    notNull(this.price, "Price is required");
+    isTrue(this.price.compareTo(BigDecimal.ZERO) > 0, "Price must be greater than zero");
+  }
+}
---
+++ src/test/java/com/sulimann/projetoddd/entity/ProductTest.java
@@ -0,0 +1,53 @@
+package com.sulimann.projetoddd.entity;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+import java.math.BigDecimal;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+public class ProductTest {
+
+  @Nested
+  class Constructor {
+
+    @Test
+    @DisplayName("Should throw exception when id is empty")
+    void shouldThrowExceptionWhenIdIsEmpty() {
+      assertThatThrownBy(() -> new Product("", "Product 1", new BigDecimal("100.00")))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("Id is required");
+    }
+
+    @Test
+    @DisplayName("Should throw exception when name is empty")
+    void shouldThrowExceptionWhenNameIsEmpty() {
+      assertThatThrownBy(() -> new Product("1", "", new BigDecimal("100.00")))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("Name is required");
+    }
+
+    @Test
+    @DisplayName("Should throw exception when price is null")
+    void shouldThrowExceptionWhenPriceIsNull() {
+      assertThatThrownBy(() -> new Product("1", "Product 1", null))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("Price is required");
+    }
+
+
+    @ParameterizedTest
+    @DisplayName("Should throw exception when price is less than zero")
+    @ValueSource(strings = { "-1", "0" })
+    void shouldThrowExceptionWhenPriceIsLessThanZero(String price) {
+      assertThatThrownBy(() -> new Product("1", "Product 1", new BigDecimal(price)))
+          .isInstanceOf(IllegalArgumentException.class)
+          .hasMessage("Price must be greater than zero");
+    }
+  }
+
+}

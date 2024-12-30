aac7a6ad351d93992889601e48c8504959ac3a6d
Rafael Sulimann <sulimannrafael@gmail.com>
2024-12-30 01:01:59 -0300

    ProductService - ajustando nome do validador do método increasePrice para ficar mais semântico

--- src/main/java/com/sulimann/projetoddd/service/ProductService.java
+++ src/main/java/com/sulimann/projetoddd/service/ProductService.java
@@ -12,7 +12,7 @@ import com.sulimann.projetoddd.entity.Product;
 public class ProductService {
 
   public static void increasePrice(Set<Product> products, BigDecimal percentage) {
-    validate(products, percentage);
+    validateIncreasePrice(products, percentage);
     products.forEach(product -> {
       var newPrice = product.getPrice()
           .add(percentage.divide(new BigDecimal("100.0")).multiply(product.getPrice()));
@@ -21,7 +21,7 @@ public class ProductService {
     });
   }
 
-  private static void validate(Set<Product> products, BigDecimal percentage) {
+  private static void validateIncreasePrice(Set<Product> products, BigDecimal percentage) {
     isFalse(products.isEmpty(), "List of products is required");
     notNull(percentage, "Percentage is required");
     isTrue(percentage.compareTo(BigDecimal.ZERO) > 0, "Percentage must be greater than zero");

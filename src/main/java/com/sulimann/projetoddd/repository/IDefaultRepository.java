1056b2d266244b27753e4ec13f248d02ecee3253
Rafael Sulimann <sulimannrafael@gmail.com>
2025-01-03 02:52:44 -0300

    IDefaultRepository - alterao nome do método save para create para melhorar a semântica

--- src/main/java/com/sulimann/projetoddd/repository/IDefaultRepository.java
+++ src/main/java/com/sulimann/projetoddd/repository/IDefaultRepository.java
@@ -1,7 +1,7 @@
 package com.sulimann.projetoddd.repository;
 
 public interface IDefaultRepository <T> {
-  void save(T entity);
+  void create(T entity);
   void update(T entity);
   void delete(T entity);
   T findById(String id);
